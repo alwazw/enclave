45a0f91666c43ec91255d41c611b85141afacd8c
alwazw <wafic@wazzan.us>
2026-07-06 03:50:53 +0000

    new sudo

--- scripts/01_privileges.sh
+++ scripts/01_privileges.sh
@@ -1,3 +1,4 @@
-echo "$USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/90-$USER-nopasswd" > /dev/null
-sudo chmod 0440 "/etc/sudoers.d/90-$USER-nopasswd"
+# Use $SUDO_USER so it targets the person who ran sudo, not root
+echo "$SUDO_USER ALL=(ALL:ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/90-$SUDO_USER-nopasswd" > /dev/null
+sudo chmod 0440 "/etc/sudoers.d/90-$SUDO_USER-nopasswd"
 echo "[SUCCESS] Passwordless sudo mapped."
